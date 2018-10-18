a01c0727293a7d411bce4a51883dc3e0517c5030
Stefan Andersson <ripcitybassworks@gmail.com>
2018-10-18 16:42:46 -0600

    added xc7_top_level.vhd

---
+++ xc7_top_level.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/18/2018 04:38:16 PM
+-- Design Name: 
+-- Module Name: xc7_top_level - xc7_top_level_arch
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity xc7_top_level is
+--  Port ( );
+end xc7_top_level;
+
+architecture xc7_top_level_arch of xc7_top_level is
+
+begin
+
+
+end xc7_top_level_arch;
