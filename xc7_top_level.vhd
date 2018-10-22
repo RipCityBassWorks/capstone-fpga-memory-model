f258cbd94cb1420547d9b98e3b576da646e92f04
Stefan Andersson <ripcitybassworks@gmail.com>
2018-10-22 16:36:01 -0600

    Added the port I/O

--- xc7_top_level.vhd
+++ xc7_top_level.vhd
@@ -32,12 +32,40 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity xc7_top_level is
---  Port ( );
+    Port(--Clock signal
+        CLK100MHZ      : in    std_logic;
+        --Switches
+        sw              : in    std_logic_vector(3 downto 0);
+        --LEDs
+        led             : out   std_logic_vector(3 downto 0);
+        --Buttons
+        btn             : in    std_logic_vector(3 downto 0);
+        --Pmod Header JA
+        ja              : out   std_logic_vector(7 downto 0);
+        --Pmod Header JB
+        jb              : out   std_logic_vector(7 downto 0);
+        --Pmod Header JC
+        jc              : out   std_logic_vector(7 downto 0);
+        --Pmod Header JD
+        jd              : out   std_logic_vector(7 downto 0);
+        --USB-UART Interface
+        uart_rxd_out    : out   std_logic;
+        uart_txd_in     : in    std_logic
+    );
 end xc7_top_level;
 
 architecture xc7_top_level_arch of xc7_top_level is
 
-begin
+signal test             : std_logic     := '0';
 
+begin
+    
+    process(CLK100MHZ)
+        begin
+            if(rising_edge(CLK100MHZ)) then
+                uart_rxd_out <= not test;
+            end if;
+    end process;
+        
 
 end xc7_top_level_arch;
