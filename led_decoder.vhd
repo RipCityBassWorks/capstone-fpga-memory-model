178cc47e1086650a04dcd3ff9679177d30760e53
Stefan Andersson <ripcitybassworks@gmail.com>
2018-11-03 17:53:33 -0600

    Added delay_counter component

--- led_decoder.vhd
+++ led_decoder.vhd
@@ -1,21 +1,16 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Stefan Andersson
 -- 
 -- Create Date: 10/28/2018 07:51:19 PM
--- Design Name: 
+-- Design Name: led_decoder.vhd
 -- Module Name: led_decoder - led_decoder_arch
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Project Name: capstone-fpga-memory-model
+-- Target Devices: XC7A35TICSG324-1L
+-- Tool Versions: Vivado 2018.2
+-- Description: Decodes an 8 bit binary number
+-- to an ouput sequence for the on board LEDs
+-- and displays the output for 4 secs
+-- Component of xc7_top_level.vhd
 ----------------------------------------------------------------------------------
 
 
@@ -26,7 +21,7 @@ entity led_decoder is
     port(
         clk             : in    std_logic;
         reset           : in    std_logic;
-        mem_block       : in    std_logic_vector(15 downto 0);
+        mem_block       : in    std_logic_vector(7 downto 0);
         led             : out   std_logic_vector(3 downto 0);               
         led0_b          : out	std_logic;
         led0_g          : out   std_logic;
@@ -41,35 +36,64 @@ entity led_decoder is
         led3_g          : out   std_logic;
         led3_r          : out   std_logic
     );
-end led_decoder;
+end  entity led_decoder;
 
 architecture led_decoder_arch of led_decoder is
     
-    signal led_vector   : std_logic_vector(15 downto 0);
+--COMPONENT DECLARATIONS
+    component delay_counter is
+    port(
+        clock           : in    std_logic;
+        reset           : in    std_logic;
+        delay_out       : out   std_logic
+    );
+    end component delay_counter;
+
+--SIGNALS  
+    signal led_vector   : std_logic_vector(7 downto 0);
+    signal delay_out    : std_logic;
     
 begin
     
+    FOUR_SEC_DELAY  :   delay_counter
+        port map(
+            clock       => clk,
+            reset       => reset,
+            delay_out   => delay_out
+        );
+
+--    process(clk, reset)
+--        begin
+--            if(reset = '0') then
+--                led_vector <= "00000000";  
+--            elsif(rising_edge(clk)) then
+--                led_vector <= mem_block;  
+--            end if;
+--    end process;
+    
+    led_vector <= mem_block;
+    
     process(clk, reset)
         begin
-            if(reset = '0') then
-                led_vector <= "0000000000000000";  
-            elsif(rising_edge(clk)) then
-                led_vector <= mem_block;  
+            if(rising_edge(clk)) then
+                if(delay_out = '1') then
+                    led0_b      <= led_vector(7);
+                    led0_r      <= led_vector(6);
+                    led1_b      <= led_vector(5);
+                    led1_r      <= led_vector(4);
+                    led2_b      <= led_vector(3);
+                    led2_r      <= led_vector(2);
+                    led3_b      <= led_vector(1);
+                    led3_r      <= led_vector(0); 
+                end if;
             end if;
     end process;
     
-        led         <= led_vector(3 downto 0);               
-        led0_b      <= led_vector(4);
-        led0_g      <= led_vector(5);
-        led0_r      <= led_vector(6);
-        led1_b      <= led_vector(7);
-        led1_g      <= led_vector(8);
-        led1_r      <= led_vector(9);
-        led2_b      <= led_vector(10);
-        led2_g      <= led_vector(11);
-        led2_r      <= led_vector(12);
-        led3_b      <= led_vector(13);
-        led3_g      <= led_vector(14);
-        led3_r      <= led_vector(15);      
-
+    led(3 downto 0)     <= led_vector(7 downto 4);
+    led0_r              <= led_vector(0);
+    led1_r              <= led_vector(1);
+    led2_r              <= led_vector(2);
+    led3_r              <= led_vector(3); 
+    
+                
 end led_decoder_arch;
